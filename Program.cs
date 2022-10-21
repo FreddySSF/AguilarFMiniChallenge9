dda8b899ec7a91b76d6ff6070537ce424a48ff56
FreddySSF <fernandoaguilarssf@gmail.com>
2022-10-21 11:05:59 -0700

    AguilarF MiniChallenge9 Endpoint

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 // Create a program that provides the user with different options of restaurants
 //  depending on what type of food they are in the mood for.
 // Provide data validation and the option to replay program.
-// Peer Reviewed By:
+// Peer Reviewed By: Richard Johnson - The program works well for the most part, as it randomly picks a restaurant using a switch statement depending on what input the user types. However, there is a slight problem if the player enters an invalid input. While it does print "Invalid Input" the first time, it will continue to the ending input if you enter an invalid input more than once, or will just go there the first time if you press enter without inputting anything, so that part could definitely use a little work. Though, aside from that, it still does what it is supposed to do.
 
 Console.Clear();
 
